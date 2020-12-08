390906ebc975ccebd383e79abb24f7935537e317
iyhunko <iyhunko@gmail.com>
2020-12-08 23:52:15 +0200

    Part3: Implement `convert()`

--- src/main/java/com/epam/rd/java/basic/practice3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part3.java
@@ -1,12 +1,38 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part3 {
 
+    public final static String FILE_PATH = System.getProperty("user.dir") + "/part3.txt";
+
+    public final static String REGEX = "[a-zA-Zа-яА-ЯіІїЇґҐ]+";
+
     public static void main(String[] args) {
-        System.out.println("ok");
+        String input = Util.getInput(FILE_PATH);
+
+        System.out.println("Result of convert:");
+        System.out.println();
+        System.out.println(convert(input));
     }
 
     public static String convert(String input) {
-        return null;
+        StringBuilder builder = new StringBuilder(input);
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(builder);
+
+        while (m.find()) {
+            if (m.group().length() > 1) {
+                String firstChar = m.group().substring(0, 1);
+                if (Character.isUpperCase(firstChar.charAt(0))) {
+                    builder.replace(m.start(), m.start() + 1, m.group().substring(0, 1).toLowerCase());
+                } else {
+                    builder.replace(m.start(), m.start() + 1, m.group().substring(0, 1).toUpperCase());
+                }
+            }
+        }
+
+        return builder.toString();
     }
 }
