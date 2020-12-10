f69613cf374c63213ccfb144774221d867c4233a
iyhunko <iyhunko@gmail.com>
2020-12-10 14:13:11 +0200

    Part4: Implement `hash()`

--- src/main/java/com/epam/rd/java/basic/practice3/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part4.java
@@ -1,12 +1,40 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Part4 {
 
-    public static void main(String[] args) {
-        System.out.println("ok");
+    public static void main(String[] args) throws NoSuchAlgorithmException  {
+        System.out.println("Result of SHA-256:");
+        System.out.println(hash("passwort", "SHA-256"));
+
+        System.out.println();
+
+        System.out.println("Result of MD5:");
+        System.out.println(hash("password", "MD5"));
     }
 
-    public static String hash(String input, String algorithm) {
-        return null;
+    public static String hash(String input, String algorithm) throws NoSuchAlgorithmException  {
+        MessageDigest digest = MessageDigest.getInstance(algorithm);
+        digest.update(input.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hash = digest.digest();
+        StringBuilder builder = new StringBuilder();
+
+        for (byte b : hash) {
+            if (b < 0) {
+                String x = Integer.toHexString(b);
+                String y = x.substring(x.length() - 2);
+                builder.append(y);
+
+                continue;
+            }
+            builder.append(Integer.toHexString(b));
+        }
+
+        return builder.toString().toUpperCase();
     }
+
 }
